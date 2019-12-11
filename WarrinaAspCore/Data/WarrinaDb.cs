3230ba8f863487ade4770bfaa79df68ed538f869
isaac <isaac.gardner@gmail.com>
2019-12-11 13:52:04 +1000

    First commit for project

---
+++ WarrinaAspCore/Data/WarrinaDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarrinaAspCore.Models;
+
+namespace WarrinaAspCore.Data
+{
+    public class WarrinaDb : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.Options != null)
+                optionsBuilder.UseSqlite("Data Source=Warrina.db");
+        }
+    }
+}
